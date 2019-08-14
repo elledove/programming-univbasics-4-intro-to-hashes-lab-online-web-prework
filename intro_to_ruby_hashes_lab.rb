389867b28ba0b90c389e84ff16a7cc4700ee743f
Lauren Dove <elledoveinc@gmail.com>
2019-08-14 03:52:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- update_counting_hash = hash[key]
+
  # update_hash = update_counting_h
- 
- if hash[key] 
+  if hash[key] 
    hash[key]+=1 else 
    puts hash[key]=1
  end
